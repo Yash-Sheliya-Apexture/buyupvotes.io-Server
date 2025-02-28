cbaf1126130b4bdb5f2e855bee65b9db892ab20a
Yash-Sheliya-Apexture <yash@apexture.in>
2025-02-28 09:23:16 +0530

    add new code

--- app.js
+++ app.js
@@ -400,7 +400,7 @@ app.use(compression());
 app.use(morgan('dev'));
 
 app.use(cors({
-    origin: ['http://localhost:5173', '*'],  // Corrected to include protocol
+    origin: ['https://redditmarketing.company', '*'],  // Corrected to include protocol
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
